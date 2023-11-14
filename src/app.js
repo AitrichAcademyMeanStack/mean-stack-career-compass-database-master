a814ec0e5e0476f5d2b76250519dae5f225ec64d
RAHUL MK <rahul.madambil123@gmail.com>
2023-11-14 23:13:45 +0530

    json data is added

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
-import express from "express";
+import express from "express"; 
 import dotenv from "dotenv";
-
-dotenv.config({
-    path:"./env"
-})
+ 
+dotenv.config()
 
 
 const app = express();
 
 app.use(express.json())
+
+app.get("/v1/api/users",(req,res)=>{
+    res.send("hello world")
+})
 
 app.listen(process.env.PORT || 5000 , () => console.log("Server up and running 🚀"));
