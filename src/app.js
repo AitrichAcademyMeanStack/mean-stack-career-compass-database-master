21ecbe7e7dfc85958cf02850f25da8f6fc96aa8c
Mohammed Faheem <faheemmint978@gmail.com>
2023-11-15 09:53:06 +0530

    command added

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from "express"; // importing express
+import dotenv from "dotenv";   // importing .env file
 
-dotenv.config({
+dotenv.config({     // configuring .env file
     path:"./env"
 })
 
 
-const app = express();
+const app = express(); 
 
-app.use(express.json())
+app.use(express.json()); // Defining miidleware
 
 app.listen(process.env.PORT || 5000 , () => console.log("Server up and running 🚀"));
