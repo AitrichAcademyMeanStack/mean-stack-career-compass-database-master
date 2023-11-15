93cff00b7a588ff92764226697405d3a67b72902
RAHUL MK <rahul.madambil123@gmail.com>
2023-11-15 16:33:30 +0530

    directory updation

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ import express from "express"; // importing express
 import dotenv from "dotenv";   // importing .env file
 import swaggerjsdoc from 'swagger-jsdoc' // importing swagger-jsdoc
 import swaggerui from 'swagger-ui-express' // imorting swagger-ui-express
-import connecttodatabase from './config/Db.js'
+import connecttodatabase from './config/db.js'
 import {errorhandler} from "./middleware/errorhandler.js";
-import jobcategoryrouter from '../src/job_category/router.js'
+import jobcategoryrouter from './job_category/router.js'
 
 dotenv.config({     // configuring .env file
     path:"./env"
