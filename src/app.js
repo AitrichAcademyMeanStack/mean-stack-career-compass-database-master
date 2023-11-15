3c7eb0d66e0096a4d297cf9bdff55401b15e06cf
RAHUL MK <rahul.madambil123@gmail.com>
2023-11-15 11:48:40 +0530

    directory structure is updated

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ import express from "express"; // importing express
 import dotenv from "dotenv";   // importing .env file
 import swaggerjsdoc from 'swagger-jsdoc' // importing swagger-jsdoc
 import swaggerui from 'swagger-ui-express' // imorting swagger-ui-express
-import connecttodatabase from './config/db.js'
-import {errorhandler} from "./middleware/errorhandler.js";
+import connecttodatabase from './config/Db.js'
+import {errorhandler} from "./middleware/ErrorHandler.js";
 
 dotenv.config({     // configuring .env file
     path:"./env"
