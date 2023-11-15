d1e421d130a3aea7aebff12f43cfca6235710b5f
Mohammed Faheem <faheemmint978@gmail.com>
2023-11-15 11:40:55 +0530

    package.json modified

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";   // importing .env file
 import swaggerjsdoc from 'swagger-jsdoc' // importing swagger-jsdoc
 import swaggerui from 'swagger-ui-express' // imorting swagger-ui-express
 import connecttodatabase from './config/db.js'
-import errorhandler from "./middleware/errorhandler.js";
+import {errorhandler} from "./middleware/errorhandler.js";
 
 dotenv.config({     // configuring .env file
     path:"./env"
--- src/middleware/errorhandler.js
+++ src/middleware/errorhandler.js
@@ -1,7 +1,8 @@
-export default errorhandler=(error,req,res,next)=>{
+const errorhandler = (error,req,res,next)=>{
     const status = error.statuscode || 500
     const message = error.message   
     res.status (status).json({message:message,status:status})
 }
 
+export {errorhandler}
 // defining global error handler
