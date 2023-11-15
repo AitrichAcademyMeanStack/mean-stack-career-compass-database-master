2292436810673657c0b79926c90ba22ccb3bb1b4
Mohammed Faheem <faheemmint978@gmail.com>
2023-11-16 01:14:18 +0530

    swagger config

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ import dotenv from "dotenv";   // importing .env file
 import swaggerjsdoc from 'swagger-jsdoc' // importing swagger-jsdoc
 import swaggerui from 'swagger-ui-express' // imorting swagger-ui-express
 import connecttodatabase from './Config/db.js'
+import path , { dirname }  from 'path';
+import { fileURLToPath } from 'url';
 import {errorhandler} from "./middleware/errorhandler.js";
 import jobcategoryrouter from './Job_Category/router.js'
 import locationRouter from "./job_location/router.js"
-
+const __dirname = dirname(fileURLToPath(import.meta.url));
 // configuring .env file
 dotenv.config({     
     path:"./env"
@@ -20,23 +22,27 @@ connecttodatabase() //connect to database
 app.use(express.json()) //defining middleware
 
 // Setup swagger
-const options = {
-    definition:{
+const swaggerDefinition = {
         openapi: "3.0.0",
         info:{
             title:"JOB_PORTAL API",
             version:"1.0.0",
             description:"API FOR MANAGING JOB_PORTAL SYSTEM"
-        }
-
-    },
-    servers:[
+        },
+    servers: [
         {
-            url: process.env.PORT
-        }
+            url: "http://localhost:7000",
+        },
     ],
-    apis:[]
+    
 }
+
+const options = {
+    swaggerDefinition,
+    apis: [
+        path.join(__dirname, "src", "job_location" , "router.js")
+    ],
+};
 
 const swaggerspecs = swaggerjsdoc(options);
 app.use('/api-docs',swaggerui.serve,swaggerui.setup(swaggerspecs));
