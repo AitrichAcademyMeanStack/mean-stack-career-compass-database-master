1fc84152dbabd83d666e0d827b1ff15d0418ca31
RAHUL MK <rahul.madambil123@gmail.com>
2023-11-22 15:31:49 +0530

    comments added in app.js file

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express"; // importing express
 import dotenv from "dotenv";   // importing .env file
 import swaggerjsdoc from 'swagger-jsdoc' // importing swagger-jsdoc
 import swaggerui from 'swagger-ui-express' // imorting swagger-ui-express
-import connecttodatabase from './Config/db.js'
+import connecttodatabase from './Config/db.js' // importing connection to database
 import path , { dirname }  from 'path'; // importing path , dirname API'S from path module
 import Notfounderror from './Exceptions/NotFoundError.js' // importing Custom Error Handler
 import { fileURLToPath } from 'url'; // importing url module
@@ -13,7 +13,7 @@ import locationRouter from "./job_location/router.js" // importing routes for lo
 import qualificationRouter from "./Qualification/Router.js" // importing routes for qualification  module
 import rolesRouter from "./Role/Router.js" // importing routes for Role  module
 import seekerrouter from './JobSeeker/Router.js' //importing routes for seeker module
-// import seekerprofile from './JobSeekerProfile/Router.js' //importing routes for job seeker profile module
+import seekerprofile from './JobSeekerProfile/Router.js' //importing routes for job seeker profile module
 import industryRouter from './Industry/Router.js'  //importing routes for Industry
 import jobProviderCompanyRouter from './JobProviderCompany/Router.js' //importing routes for JobProviderCompany
 const __dirname = dirname(fileURLToPath(import.meta.url)); // Configuring dirname path
@@ -71,10 +71,11 @@ app.use('/api-docs',swaggerui.serve,swaggerui.setup(swaggerspecs));
 // Defining routes
 app.use('/api/v1/job-categories',jobcategoryrouter); // configuring routes for Job Category
 app.use('/api/v1/locations',locationRouter); // configuring routes for Location
-app.use('/api/v1/qualifications',qualificationRouter)
+app.use('/api/v1/qualifications',qualificationRouter) //configuring routes for qulaification
 app.use('/api/v1/skills',skillrouter); // configuring routes for skill
 app.use('/api/v1/roles', rolesRouter); // Configuring routes for roles
 app.use('/api/v1/jobseekers',seekerrouter) //configuring routes for seeker
+app.use('/api/v1/jobseekers',seekerprofile) //configuring routes for seeker profile
 app.use('/api/v1/industries', industryRouter) // Configuring routes for industry
 app.use('/api/v1/jobProviderCompanies', jobProviderCompanyRouter) // Configuring routes for JobProviderCompany
 
