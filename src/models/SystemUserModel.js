8ac61c7c33ffae023c562ab33e2376e2cfa9c269
RAHUL MK <rahul.madambil123@gmail.com>
2023-11-28 11:02:21 +0530

    update in system user

--- src/models/SystemUserModel.js
+++ src/models/SystemUserModel.js
@@ -4,11 +4,11 @@ import mongoose ,{Schema} from 'mongoose' //importing mongoose
 //schema for systemuser
 const systemuserschema = new Schema (
     {
-        firstName: { type: String,ref:'jobseeker', required: true },
+        firstName: { type: String,required: true },
         role: { type: String, enum: ["Platform Admin","Company Admin","Hiring Manager", "Job Seeker"], required: true },
-        lastName: { type: String,ref:'jobseeker', required: true },
-        email:{ type: String,ref:'jobseeker', required: true },
-        phone: { type: String,ref:'jobseeker', required: true }
+        lastName: { type: String,required: true },
+        email:{ type: String,required: true },
+        phone: { type: String,required: true }
       },{
         versionKey:false
     }
