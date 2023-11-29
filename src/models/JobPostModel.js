1c7d4a58dca5dfa9dbe2182a96706e2435675a31
Mohammed Faheem <faheemmint978@gmail.com>
2023-11-29 16:13:46 +0530

    fix: bug fix in JobPostModel

--- src/models/JobPostModel.js
+++ src/models/JobPostModel.js
@@ -1,21 +1,20 @@
-import mongoose , {Mongoose, Schema} from 'mongoose';
-import JobProviderCompanySchema from './JobProviderCompanyModel.js'
+import mongoose , {Schema} from 'mongoose';
 const jobPostSchema = new Schema(
     {
         jobTitle:{type: String},
         jobSummary:{type:String},
-        jobLocation:{type:mongoose.Schema.Types.ObjectId,ref:"Location"},
-        company:{type:mongoose.Schema.Types.ObjectId,ref:"JobProviderCompany"},
-        category:{type:mongoose.Schema.Types.ObjectId,ref:"Job_Category"},
-        qualifications:{type:mongoose.Schema.Types.ObjectId,ref:"Qualification"},
-        skills:{type:mongoose.Schema.Types.ObjectId,ref:"skills"},
-        industry:{type:mongoose.Schema.Types.ObjectId,ref:"Industry"},
+        jobLocation:{type:mongoose.Types.ObjectId,ref:"Location"},
+        company:{type:mongoose.Types.ObjectId,ref:"JobProviderCompany"},
+        category:{type:mongoose.Types.ObjectId,ref:"Job_Category"},
+        qualifications:{type:mongoose.Types.ObjectId,ref:"Qualification"},
+        skills:{type:mongoose.Types.ObjectId,ref:"skills"},
+        industry:{type:mongoose.Types.ObjectId,ref:"Industry"},
         jobResponsibilities:{type:String},
         postedBy:{type:String},
         postedDate:{type:Date, default:Date.now()}
     },{versionKey:false}
 )
 
-const collectionName = "Job Post"
+const collectionName = "Job Post";
 const JobPost = mongoose.model("JobPost", jobPostSchema , collectionName);
 export default JobPost
