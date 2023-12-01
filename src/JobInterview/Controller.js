8db06b139652cddaae031fc92e5cc170447138ba
Mohammed Faheem <faheemmint978@gmail.com>
2023-12-01 16:34:47 +0530

    feat: JobInterview modified

--- src/JobInterview/Controller.js
+++ src/JobInterview/Controller.js
@@ -1,11 +1,13 @@
 import asyncHandler from "../utils/asyncerrorhandler";
+import jobInterviewService from "./Service.js"
 
 
+// GetAllJobInterviews
+const getAllJobInterview = asyncHandler( async(req , res) => {
+    const data = await jobInterviewService.getAllJobInterview()
+    res.status(200).json(data)
 
-const getJobInterview = asyncHandler( async(req , res) => {
-    
 })
-
 
 
 // Adding JobInterview
@@ -15,3 +17,11 @@ const addJobInterview = asyncHandler( async(req , res) => {
     res.status(201).json(interview)
 
 })
+
+const deleteJobInterview  = asyncHandler( async(req , res) => {
+    const JobInterviewID = req.params.id;
+    const deleteInterview = await jobInterviewService.deleteJobInterview(JobInterviewID);
+    res.status(200).json(deleteInterview)
+})
+
+export default {getAllJobInterview , addJobInterview}
