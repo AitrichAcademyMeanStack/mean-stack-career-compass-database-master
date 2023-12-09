22f46140b5ee30a023ee2cf5810c30823b7438ef
divyasushinanay <divyasushin.aitrich@gmail.com>
2023-12-09 09:52:30 +0530

    platform admin is updated

--- src/models/PlatformAdminModel.js
+++ src/models/PlatformAdminModel.js
@@ -3,9 +3,11 @@ import mongoose,{Schema} from "mongoose";//importing mongoose
 
 //platformadmin schema model
 const adminSchema = new Schema(
-    {
-        userName:{type: String},
-        password:{type:String},
+    {   firstName: { type: String, required: true ,trim:true},
+    lastName: { type: String, required: true,trim:true },
+    userName: {type: String,required:true,trim:true},
+    email: { type: String, required: true,trim:true },
+    phone: { type: Number, required: true,trim:true },
         role: {
             type: String,
             default: "Platform Admin",
