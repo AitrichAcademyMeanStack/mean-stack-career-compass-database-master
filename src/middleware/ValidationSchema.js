0885d6a14b385b36a1384f95567cffb1bf039ea4
divyasushinanay <divyasushin.aitrich@gmail.com>
2024-01-08 10:14:44 +0530

    jobpost validation is added

--- src/middleware/ValidationSchema.js
+++ src/middleware/ValidationSchema.js
@@ -198,7 +198,7 @@ const companyUserValidate = Joi.object({
       "Phone Number should not be empty. Please provide a value for the Phone field.",
   })
 
-})
+});
 
 const jobseekervalidation = Joi.object({
   firstName: Joi.string()
@@ -259,6 +259,62 @@ const jobseekervalidation = Joi.object({
       "number.empty":"Phone Number should not be empty. Please provide a value for the Phone field.",
     })
 })
+//validating jobpost
+ const jobPostvalidation=Joi.object({
+ jobTitle:Joi.string()
+ .required()
+ .min(3)
+ .max(30)
+ .pattern(/^[a-zA-Z0-9]+$/)
+ .messages({
+   "string.pattern.base": "jobTitle field only contains alphabets and numbers without spaces",
+   "string.min": "jobTitle field must be at least 3 characters long",
+   "any.required": "jobTitle field is required",
+   "string.empty": "jobTitle field should not be empty. Please provide a value for the jobTitle field.",
+ }),
+
+ jobSummary:Joi.string()
+ .required()
+ .min(20)
+ .max(100)
+ .pattern(/^[a-zA-Z0-9]+$/)
+ .messages({
+   "string.pattern.base": "jobSummary field only contains alphabets and numbers without spaces",
+   "string.min": "jobSummary field must be at least 3 characters long",
+   "any.required": "jobSummary field is required",
+   "string.empty": "jobSummary field should not be empty. Please provide a value for the jobSummary field.",
+ }),
+ jobResponsibilities:Joi.string()
+ .required()
+ .min(20)
+ .max(100)
+ .pattern(/^[a-zA-Z0-9]+$/)
+ .messages({
+   "string.pattern.base": "jobResponsibilities field only contains alphabets and numbers without spaces",
+   "string.min": "jobResponsibilities field must be at least 3 characters long",
+   "any.required": "jobResponsibilities field is required",
+   "string.empty": "jobResponsibilities field should not be empty. Please provide a value for the jobResponsibilities field.",
+ }),
+})
+ //Validatingjobapplication
+ const jobApplicationvalidation=Joi.object({
+  
+  coverletter:Joi.string()
+
+  .required()
+  .min(20)
+  .max(100)
+  .pattern(/^[a-zA-Z0-9]+$/)
+  .messages({
+    "string.pattern.base": "coverletter field only contains alphabets and numbers without spaces",
+    "string.min": "coverletter field must be at least 3 characters long",
+    "any.required": "coverletter field is required",
+    "string.empty": "coverletter field should not be empty. Please provide a value for the coverletter field.",
+  }),
 
 
-export  {authschema,experiencevalid,jobProviderValidate,jobseekervalidation,companyUserValidate}
+
+
+ })
+
+export  {authschema,experiencevalid,jobProviderValidate,jobseekervalidation,companyUserValidate,jobPostvalidation,jobApplicationvalidation }
